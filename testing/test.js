25095e415e1bdc31fddff3873d32200772145233
Savalan Suleymanli <98057609+Savalan11@users.noreply.github.com>
2022-04-20 17:58:07 +0200

    return data and register, update and delete new users new

--- testing/test.js
+++ testing/test.js
@@ -117,6 +117,42 @@ app = express(),
     }
   })
 
+  // create
+  app.post('/users/:id/:movieTitle', (req, res) => {
+    const { id, movieTitle } = req.params;
+    let user = users.find( user => user.id == id );
+    if (user) {
+      user.favoriteMovies.push(movieTitle);
+      res.status(200).send(`${movieTitle} has been added to user $(id)'s array`);;
+    } else {
+      res.status(400).send('no such user')
+    }
+  })
+
+  // delete
+  app.delete('/users/:id/:movieTitle', (req, res) => {
+    const { id, movieTitle } = req.params;
+    let user = users.find( user => user.id == id );
+    if (user) {
+      user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle);
+      res.status(200).send(`${movieTitle} has been removed from user $(id)'s array`);;
+    } else {
+      res.status(400).send('no such user')
+    }
+  })
+
+  // delete/deregister
+  app.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+    let user = users.find( user => user.id == id );
+    if (user) {
+      users = users.filter( user => user.id != id);
+      res.status(200).send(`user $(id) has been deleted`);;
+    } else {
+      res.status(400).send('no such user')
+    }
+  })
+
 // READ MOVIES
 app.get('/movies', (req, res) => {
   res.status(200).json(movies);
